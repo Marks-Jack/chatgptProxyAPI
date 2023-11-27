b1d8aaa64000c6c2fd87294558806d216d6b2a65
Join.Home <49591434+Marks-Jack@users.noreply.github.com>
2023-11-27 16:04:24 +0800

    Update cf_worker_old.js

--- cf_worker_old.js
+++ cf_worker_old.js
@@ -1,4 +1,4 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
+const TELEGRAPH_URL = 'https://open.gfwdemoeuorg.workers.dev';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
